606b06d6599e49e004abedea8e624b9659eaf3cc
Vish O <v.opatha@gmail.com>
2021-07-17 12:16:50 +0930

    Create basic user interface for search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,14 @@ var searchBtn =$('#search');
 //var cityLatitute = 0;
 //var cityLongitude = 0;
 var cityLatitute=0; var cityLongitude=0;
+var currentDiv=$('main').children[1];
 
 var temp;
 var wind;
 var humidity;
 var uvIndex;
 var cloud;
-
-//forecast = http://api.openweathermap.org/data/2.5/forecast?q=Adelaide&cnt=5&appid=975b6c913fd1bac8d093c8550b538f26
-
-
-
-
-//---->var fetU="http://api.openweathermap.org/data/2.5/forecast/daily?q=Adelaide&cnt=5&appid="+ APIKey;
-//http://api.openweathermap.org/data/2.5/forecast/daily?q=Adelaide&cnt=5&appid=975b6c913fd1bac8d093c8550b538f26
-//-----
-// fetch(fetU)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       }
-//     );
+var currentWeather=[];
 
 
 
@@ -46,6 +31,7 @@ searchBtn.on('click',function(event)
     else
     {
         getCurrentWeather(searchTerm);
+        getWeatherForecast(searchTerm);
         
         //displayCurrentWeather();
     }
@@ -68,6 +54,7 @@ function getCurrentWeather(searchTerm)
                         wind=data.wind.speed;
                         humidity=data.main.humidity;
                         var weatherCon =data.weather[0].main;
+                        var todayDate=moment().format("dd mm yy");
                         
                         cityLatitute = JSON.stringify(data.coord.lat);
                         cityLongitude= JSON.stringify(data.coord.lon);
@@ -77,7 +64,35 @@ function getCurrentWeather(searchTerm)
                         console.log(weatherCon);
                         console.log(cityLatitute+ " "+cityLongitude);
 
+                        currentWeather.push([searchTerm][temp,wind,humidity,weatherCon]);
+                        console.log(currentWeather);
+
+
+                        // var cityH2=$('<h2>');
+                        // var temperatureP=$('<p>');
+                        // var windP=$('<p>');
+                        // var humidityP=$('<p>');
+                        // // var uviP=$('<p>');
+
+                        
+
+    
+                        cityH2.text(searchTerm + todayDate+weatherCon);
+                        temperatureP.text("Temperature : "+temp+ "<span>&#8451;</span>C");
+                        windP.text("Wind :"+wind+" MPH");
+                        humidityP.text("Humidity :"+humidity+"%");
+
+                        console.log(humidityP);
+
+                        // currentDiv.append(cityH2);
+                        // currentDiv.append(temperatureP);
+                        // currentDiv.append(windP);
+                        // currentDiv.append(humidityP);
+
+
                         getUVIndex(cityLatitute,cityLongitude);
+                       
+                        
                         
                     });
                 }
@@ -111,6 +126,11 @@ function getUVIndex(lat,lon)
             response.json().then(function (data) {
             uvIndex=data.daily[0].uvi;
             console.log("uvi" +uvIndex);
+            var uviP=$('<p>');
+            uviP.text("UV Index"+uvIndex);
+            currentDiv.append(uviP);
+
+                     
             });
         }
 
@@ -131,80 +151,52 @@ function getUVIndex(lat,lon)
 }
 
 //########## This function is used to obtain the weather forecast for five days for a given city ###########
+function getWeatherForecast(searchTerm)
+{
+    //var forecastUrl="http://api.openweathermap.org/data/2.5/forecast/daily?q="+searchTerm+"&cnt=5&appid="+ APIKey;
+    var forecastUrl="http://api.openweathermap.org/data/2.5/forecast?q="+searchTerm+"&cnt=5&units=metric&appid="+APIKey;
+
+    fetch(forecastUrl).then(function (response){
+        if(response.ok)
+        {
+            response.json().then(function (data) {
+            console.log(data);
+            });
+        }
+
+        else if(response.status==404)
+        {
+            window.alert("City does not exist");
+        }
+
+        else
+        {
+            window.alert("Error"+response.statusText);
+        }
+    })
+    .catch(function (error)
+    {
+        alert("Unable to connect to Weather");
+    });
+
+
+}
+
+function displayWeatherData()
+{
+    var cityH2=$('<h2>');
+    var temperatureP=$('<p>');
+    var windP=$('<p>');
+    var humidityP=$('<p>');
+    var uviP=$('<p>');
+    
+    cityH2.text()
+
+}
 
 
 
 
-
-
-// fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           displayRepos(data, user);
-//         });
-//       } else {
-//         alert('Error: ' + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert('Unable to connect to GitHub');
-//     });
-//var fetchUrl="http://api.openweathermap.org/data/2.5/weather?q=" + "Adelaide"+ "&units=metric&lat="+latitute+"&lon="+longitute+"&appid=" + APIKey;
-//var fetchUrl1="https://api.openweathermap.org/data/2.5/onecall?lat=6.9319&lon=79.8478&exclude=daily,minutely,hourly,alerts&appid=975b6c913fd1bac8d093c8550b538f26";
-// console.log(fetchUrl1);
-// fetch(fetchUrl1)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//   console.log(data);
-  
-
-// });
-
-//     }
-
-// var fetchUrl="http://api.openweathermap.org/data/2.5/weather?q=" + "colombo"+ "&units=metric&appid=" + APIKey;
-
-
-// console.log(fetchUrl);
-// fetch(fetchUrl)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//   console.log(data);
-//   temp=data.main.temp;
-//   wind=data.wind.speed;
-//   humidity=data.main.humidity;
-//   var weatherCon =data.weather[0].main;
-//   var uvindex=0; //need to find this
-//   var latitute = data.coord.lat;
-//   var longitute= data.coord.lon;
-//   console.log(temp)
-//   console.log(wind);
-//     console.log(humidity);
-//     console.log(weatherCon);
-//     console.log (latitute + "  "+longitute);
-
-// });
-
-//====var fetchUrl1="https://api.openweathermap.org/data/2.5/onecall?lat=6.9319&lon=79.8478&exclude=daily,minutely,hourly,alerts&appid=975b6c913fd1bac8d093c8550b538f26";
-// console.log(fetchUrl1);
-// fetch(fetchUrl1)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//   console.log(data);
-  
-
-// });
-
-
-
-// });
 
 
 
