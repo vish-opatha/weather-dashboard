81e8e42a6dc7b8eb3950df898212b0b76f620bfa
Vish O <v.opatha@gmail.com>
2021-07-17 14:59:37 +0930

    Update current weather data display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 var APIKey="975b6c913fd1bac8d093c8550b538f26";
-var searchCity = $('input[name="city"]');
+var searchCityInput = $('input[name="city"]');
 var searchBtn =$('#search');
 //var cityLatitute = 0;
 //var cityLongitude = 0;
 var cityLatitute=0; var cityLongitude=0;
-var currentDiv=$('main').children[1];
+var weatherDisplay=$('#display-section');
+var searchTerm;
 
 var temp;
 var wind;
 var humidity;
 var uvIndex;
 var cloud;
+var weatherCon;
+var todayDate=moment().format("DD/MM/YYYY");
+
 var currentWeather=[];
 
 
@@ -19,10 +23,8 @@ var currentWeather=[];
 searchBtn.on('click',function(event)
 {
     event.preventDefault();
-    var searchTerm = searchCity.val();
-    
-    console.log(searchCity.val());//**** */
-
+    searchTerm = searchCityInput.val();
+        
     if(searchTerm==="")
     {
         window.alert("Please enter a city to obtain weather data.");
@@ -31,7 +33,7 @@ searchBtn.on('click',function(event)
     else
     {
         getCurrentWeather(searchTerm);
-        getWeatherForecast(searchTerm);
+        //getWeatherForecast(searchTerm);
         
         //displayCurrentWeather();
     }
@@ -42,7 +44,7 @@ function getCurrentWeather(searchTerm)
 {
     var currentDataUrl="http://api.openweathermap.org/data/2.5/weather?q=" + searchTerm+ "&units=metric&appid=" + APIKey;
     
-    console.log(currentDataUrl); //==================================
+    //console.log(currentDataUrl); //==================================
 
         fetch(currentDataUrl)
             .then(function (response){
@@ -53,44 +55,18 @@ function getCurrentWeather(searchTerm)
                         temp=data.main.temp;
                         wind=data.wind.speed;
                         humidity=data.main.humidity;
-                        var weatherCon =data.weather[0].main;
-                        var todayDate=moment().format("dd mm yy");
+                        weatherCon =data.weather[0].description;
+                        console.log(todayDate);
                         
                         cityLatitute = JSON.stringify(data.coord.lat);
                         cityLongitude= JSON.stringify(data.coord.lon);
-                        console.log(temp)
-                        console.log(wind);
-                        console.log(humidity);
-                        console.log(weatherCon);
-                        console.log(cityLatitute+ " "+cityLongitude);
+                        
+                        getUVIndex(cityLatitute,cityLongitude);
+                        console.log(uvIndex);
 
-                        currentWeather.push([searchTerm][temp,wind,humidity,weatherCon]);
-                        console.log(currentWeather);
-
-
-                        // var cityH2=$('<h2>');
-                        // var temperatureP=$('<p>');
-                        // var windP=$('<p>');
-                        // var humidityP=$('<p>');
-                        // // var uviP=$('<p>');
+                        displayCurrentWeather(searchTerm,temp,wind,humidity,weatherCon,uvIndex);
 
                         
-
-    
-                        cityH2.text(searchTerm + todayDate+weatherCon);
-                        temperatureP.text("Temperature : "+temp+ "<span>&#8451;</span>C");
-                        windP.text("Wind :"+wind+" MPH");
-                        humidityP.text("Humidity :"+humidity+"%");
-
-                        console.log(humidityP);
-
-                        // currentDiv.append(cityH2);
-                        // currentDiv.append(temperatureP);
-                        // currentDiv.append(windP);
-                        // currentDiv.append(humidityP);
-
-
-                        getUVIndex(cityLatitute,cityLongitude);
                        
                         
                         
@@ -100,7 +76,7 @@ function getCurrentWeather(searchTerm)
                 else if(response.status==404)
                 {
                     //Window.alert('Error: ' + response.statusText);
-                    window.alert("City does not exist");
+                    window.alert("This city does not exist");
                 }
 
                 else
@@ -110,27 +86,58 @@ function getCurrentWeather(searchTerm)
             })
             .catch(function (error)
             {
-                alert("Unable to connect to Weather");
+                alert("Error :"+error);
             });
            
 }
 
+function displayCurrentWeather(city,temp,wind,humidity,weatherCon,uvIndex)
+{
+    var cityH2=$('<h2>');
+    var temperatureP=$('<p>');
+    var windP=$('<p>');
+    var humidityP=$('<p>');
+    var uviP=$('<p>');
+
+                        
+                        //=================
+    
+    cityH2.text(city + todayDate+ weatherCon);
+    temperatureP.text("Temperature : "+temp);
+    windP.text("Wind :"+wind+" MPH");
+    humidityP.text("Humidity :"+humidity+"%");
+    uviP.text("UV Index :"+uvIndex);
+
+    
+    weatherDisplay.append(cityH2);
+    weatherDisplay.append(temperatureP);
+    weatherDisplay.append(windP);
+    weatherDisplay.append(humidityP);
+    weatherDisplay.append(uviP);
+
+
+
+
+
+}
 //########## This function is used to obtain UV Index of the given city ###########
 function getUVIndex(lat,lon)
 {
-    var uviUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat+ "&lon="+ lon+ "&exclude=current,minutely,hourly,alerts&appid=" + APIKey;
-
+    //var uviUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat+ "&lon="+ lon+ "&exclude=current,minutely,hourly,alerts&appid=" + APIKey;
+    var uviUrl="https://api.openweathermap.org/data/2.5/onecall?lat=-33.8679&lon=151.2073&exclude=current,minutely,hourly,alerts&appid=" + APIKey;
+    var uvIndex_local;
     fetch(uviUrl).then(function (response){
         if(response.ok)
         {
             response.json().then(function (data) {
-            uvIndex=data.daily[0].uvi;
-            console.log("uvi" +uvIndex);
-            var uviP=$('<p>');
-            uviP.text("UV Index"+uvIndex);
-            currentDiv.append(uviP);
+            uvIndex_local=data.daily[0].uvi;
+            console.log("uvi" +uvIndex_local);
+            uvIndex=uvIndex_local;
+            // var uviP=$('<p>');
+            // uviP.text("UV Index"+uvIndex);
+            // currentDiv.append(uviP);
 
-                     
+            // return uvIndex_local;         
             });
         }
 
