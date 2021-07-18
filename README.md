f4a400917c5716262af79398b50a6cdc95468ff9
Vish O <v.opatha@gmail.com>
2021-07-18 20:02:33 +0930

    Update forecast and UV Index fns

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
 
 Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
-
+https://www.verywellhealth.com/know-your-uv-index-1069524
 ## User Story
 
 ```
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var APIKey="975b6c913fd1bac8d093c8550b538f26";
+
 var searchCityInput = $('input[name="city"]');
 var searchBtn =$('#search');
 //var cityLatitute = 0;
@@ -38,6 +38,7 @@ searchBtn.on('click',function(event)
     {
         getCurrentWeather(searchTerm);
         getWeatherForecast(searchTerm);
+       
         
         //displayCurrentWeather();
     }
@@ -59,17 +60,21 @@ function getCurrentWeather(searchTerm)
                         temp=data.main.temp;
                         wind=data.wind.speed;
                         humidity=data.main.humidity;
-                        weatherCon =data.weather[0].description;
+                        // weatherCon =data.weather[0].description;
+                        weatherCon =data.weather[0].icon;
                         console.log(todayDate);
+                        console.log("=======================");
+                        console.log(data);
                         
                         cityLatitute = JSON.stringify(data.coord.lat);
                         cityLongitude= JSON.stringify(data.coord.lon);
                         saveCurrentWeatherData(searchTerm);
                         
-                        getUVIndex(cityLatitute,cityLongitude);
+                        
                         
 
                         displayCurrentWeather(searchTerm,temp,wind,humidity,weatherCon,uvIndex);
+                        getUVIndex(cityLatitute,cityLongitude);
 
                         
                        
@@ -99,17 +104,20 @@ var count=0;
 function displayCurrentWeather(city,temp,wind,humidity,weatherCon,uvIndex)
 {
 
-   
-    headerCity.text(city + todayDate+ weatherCon);
+    var weatherIcon=$('<img>');
+    headerCity.text(city +" "+ todayDate+" ");
     tempSpan.text(temp);
     windSpan.text(wind);
     humiditySpan.text(humidity);
    
     var cityButton=$('<button>');
+    var headerT=$('#header-tag');
     cityButton.text(city);
     cityButton.attr("city",city);
     cityButton.addClass('btn btn-secondary btn-sm');
-
+    weatherIcon.attr("src","http://openweathermap.org/img/w/"+weatherCon+".png");
+    headerT.append(weatherIcon);
+    //headerCity.append(weatherIcon);
   
     var buttons=$('#search-history');
 
@@ -143,12 +151,27 @@ function getUVIndex(lat,lon)
             uvIndex_local=data.daily[0].uvi;
             console.log(uvIndex+"UV INdex line 136");
             console.log("uvi" +uvIndex_local);
-            uvIndex=uvIndex_local;
-            // var uviP=$('<p>');
-            // uviP.text("UV Index"+uvIndex);
-            // currentDiv.append(uviP);
+            uviSpan.text(uvIndex_local);
+           
+            if(uvIndex_local<3)
+            {
+                uviSpan.css("background-color", "green");
+            }
 
-            // return uvIndex_local;         
+            else if(uvIndex_local>=3 && uvIndex_local<6)
+            {
+                uviSpan.css("background-color", "yellow");
+            }
+
+            else
+            {
+                uviSpan.css("background-color", "red");
+            }
+            
+            
+
+
+
             });
         }
 
@@ -171,14 +194,52 @@ function getUVIndex(lat,lon)
 //########## This function is used to obtain the weather forecast for five days for a given city ###########
 function getWeatherForecast(searchTerm)
 {
-    //var forecastUrl="http://api.openweathermap.org/data/2.5/forecast/daily?q="+searchTerm+"&cnt=5&appid="+ APIKey;
     var forecastUrl="http://api.openweathermap.org/data/2.5/forecast?q="+searchTerm+"&units=metric&appid="+APIKey;
-
+    var fcDate; var fcTemp; var fcHumidity; var fcDescription; var fcWind;
     fetch(forecastUrl).then(function (response){
         if(response.ok)
         {
             response.json().then(function (data) {
-            // for (i)
+            var numberOfForecasts = data.list.length;
+            var timeStamp=data.list[numberOfForecasts-1].dt_txt.split(" ");
+            var a =timeStamp[1];
+        
+            
+            var fcDays=["day1","day2","day3","day4","day5"];
+            var j=0;
+            for(i=0;i<40;i++)
+            {
+                
+                var b=data.list[i].dt_txt.split(" ");
+                if(b[1]===a)
+                {
+                    var dayTag ="#"+fcDays[j];
+                    var dayCounter=1; j++;
+                    fcDate=b[0];
+                    fcTemp=data.list[i].main.temp;
+                    fcHumidity=data.list[i].main.humidity;
+                    // fcDescription=data.list[i].weather[i].description;
+                    fcDescription=data.list[i].weather[0].icon;
+                    fcWind=data.list[i].wind.speed;
+                    console.log(data);
+
+                   
+                    var fcDivision =$(dayTag);
+                    fcDivision.children().eq(0).children().eq(0).text(fcDate);
+                    
+                    fcDivision.children().eq(2).children().eq(0).text(fcTemp);
+                    fcDivision.children().eq(3).children().eq(0).text(fcWind);
+                    fcDivision.children().eq(4).children().eq(0).text(fcHumidity);
+                    var fcIcon=fcDivision.children().eq(1);
+                    fcIcon.attr("src","http://openweathermap.org/img/w/"+fcDescription+".png");
+
+                   
+                    dayCounter++;
+
+                }
+          
+
+            }
             });
         }
 
